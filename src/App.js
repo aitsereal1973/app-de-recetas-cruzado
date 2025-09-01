f37f2d6703c2f4d9387c3365b71dca7d0699c8e0
aitsereal1973 <odidoquinom@hotmail.com>
2025-09-01 10:35:48 -0500

    feat: initial commit for src/App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Bienvenido a la App de Recetas Cruzado</Text>
+    </View>
+  );
+};
+
+export default App;
